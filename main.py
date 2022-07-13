db07eaca251ab08df510c6643f770ea3987131b8
erosCarhuancho <eros.carhuancho@utec.edu.pe>
2022-07-12 21:44:27 -0500

    prueba1

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-def suma(num1,num2):
-    return num1+num2
+from flask import Flask, jsonify
+from mensajes import topicos
+# create the Flask app
+app = Flask(__name__)
 
-def main():
-    print(suma(3,5))
-    
-if __name__ == "__main__":
-    main()
+@app.route('/mensaje', methods=['GET'])
+def getMensajes():
+    return jsonify({'mensajes': topicos})
+
+@app.route('/mensaje/<string:mensaje_topico>')
+def getMensaje(mensaje_topico):
+    mensajes = [x for x in topicos if x['topico'] == mensaje_topico.lower()]
+    if (len(mensajes) > 0):
+        return jsonify({'Mensaje': mensajes})
+    return jsonify({'Mensaje': 'El mensaje no se obtuvo'})
+
+if __name__ == '__main__':
+
+    app.run(debug=True, port=5000)
--- test.py
+++
@@ -1,4 +0,0 @@
-import pytest
-from main import *
-def test_answer():
-    assert suma(3,5) == 8
